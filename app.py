3bb1d58665ec9610f72754bee27462bb4902afe5
Jazmin Ivy <princessivy07@yahoo.com>
2023-03-13 11:24:54 -0500

    Updated SongSchema class

--- app.py
+++ app.py
@@ -37,8 +37,19 @@ class Song(db.Model):
 
 # Schemas
 class SongSchema(ma.Schema):
+    id = fields.Integer(primary_key = True)
+    title = fields.String(required = True)
+    artist = fields.String(required = True)
+    album = fields.String(required = True)
+    release_date = fields.Date()
+    genre = fields.String(required = True)
+
     class Meta:
         fields = ("title", "artist", "album", "release_date", "genre")
+
+    @post_load
+    def create_song(self, data, **kwargs):
+        return Song(**data)
 
 song_schema = SongSchema()
 songs_schema = SongSchema(many = True)
